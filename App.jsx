424c7116bb5f9a9d1a85e35da9027b0c70c939ef
Latifa-ka <85689676+Latifa-ka@users.noreply.github.com>
2023-04-11 22:16:24 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import Header from ".Header";
+function App(){return <Heading />;}
+export default App;
---
+++ Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+function Haeding(){return(<div>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Latifa Khalifa</title>
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400&family=Mochiy+Pop+P+One&family=Montserrat:wght@100;200;300;500;700;900&family=Sacramento&display=swap" rel="stylesheet">
+  <!-- styles sheet -->
+  <link rel="stylesheet" href="./styles.css">
+  <link rel="icon" href="../images/businesswoman.png">
+</head>
+  </div>
+);}
+export default Header;
